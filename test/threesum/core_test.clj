fdd7c4a5108c4a8937319e582167c95b43174b88
Graeme Foster <graemef@gmail.com>
2014-03-08 09:21:42 +0000

    Added more merge? facts

--- test/threesum/core_test.clj
+++ test/threesum/core_test.clj
@@ -8,6 +8,10 @@
              (merge? 2 1) => true)
        (fact "pairs of no less than 3 can merge"
              (merge? 3 3) => true
-             (merge? 6 6) => true
+             (merge? 6 6) => true)
+       (fact "pairs of 1 or 2 can't merge"
              (merge? 1 1) => false
-             (merge? 2 2) => false))
+             (merge? 2 2) => false)
+       (fact "different numbers can't merge"
+             (merge? 3 6) => false
+             (merge? 6 12) => false))
