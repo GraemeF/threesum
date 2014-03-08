3a78ea6259991f1b4cb047956e5905bc2a8243e8
Graeme Foster <graemef@gmail.com>
2014-03-08 13:41:57 +0000

    Squash stuff.

--- src/threesum/core.clj
+++ src/threesum/core.clj
@@ -15,16 +15,20 @@
   (doseq [row board]
     (println (format-row row))))
 
-(defn sum? [a b]
-  (or (and (> a 2) (= a b))
+(defn squashable? [a b]
+  (or (nil? a)
+      (and (> a 2) (= a b))
       (= 3 (+ a b))))
 
-(def sum +)
+(defn squash [a b]
+  (if (nil? b)
+    nil
+    (+ (or a 0) b)))
 
-(defn shift [row summable? sum]
+(defn shift [row squashable? squash]
   (let [shifted (rest (conj row nil))]
-    (if (summable? (first row) (second row))
-      (into [(sum (first row) (second row))] (rest shifted))
+    (if (squashable? (first row) (second row))
+      (into [(squash (first row) (second row))] (rest shifted))
       shifted)))
 
 ;;(print-board board)
--- test/threesum/core_test.clj
+++ test/threesum/core_test.clj
@@ -2,27 +2,34 @@
   (:use [midje.sweet])
   (:require [threesum.core :refer :all]))
 
-(facts "about `sum?`"
-       (fact "1 and 2 can sum"
-             (sum? 1 2) => true
-             (sum? 2 1) => true)
-       (fact "pairs of no less than 3 can sum"
-             (sum? 3 3) => true
-             (sum? 6 6) => true)
-       (fact "pairs of 1 or 2 can't sum"
-             (sum? 1 1) => false
-             (sum? 2 2) => false)
-       (fact "different numbers can't sum"
-             (sum? 3 6) => false
-             (sum? 6 12) => false))
+(facts "about `squashable?`"
+       (fact "nil is always squashable"
+             (squashable? nil :a) => true)
+       (fact "1 and 2 can squash"
+             (squashable? 1 2) => true
+             (squashable? 2 1) => true)
+       (fact "pairs of no less than 3 can squash"
+             (squashable? 3 3) => true
+             (squashable? 6 6) => true)
+       (fact "pairs of 1 or 2 can't squash"
+             (squashable? 1 1) => false
+             (squashable? 2 2) => false)
+       (fact "different numbers can't squash"
+             (squashable? 3 6) => false
+             (squashable? 6 12) => false))
 
-(facts "about `sum`"
+(facts "about `squash`"
        (fact "produces sum"
-             (sum 1 2) => 3))
+             (squash 1 2) => 3)
+       (fact "replaces nil with number"
+             (squash nil 5) => 5)
+       (fact "squashes nils together"
+             (squash nil nil) => nil))
 
 (facts "about `shift`"
-       (fact "moves into empty cell when not summable"
-             (shift [nil :a nil nil] (constantly false) (constantly :x)) => [:a nil nil nil]
-             (shift [nil nil nil :a] (constantly false) (constantly :x)) => [nil nil :a nil])
-       (fact "sums at the edge when summable"
+       (fact "discards first nil cell when not squashable"
+             (shift [nil :a nil nil] #(= [nil :a] [%1 %2]) (constantly :a)) => [:a nil nil nil]
+             (shift [nil nil nil :a] (constantly false) (constantly :x)) => [nil nil :a nil]
+             (shift [:a :b :c :d] #(= [:a :b] [%1 %2]) (constantly :ab)) => [:ab :c :d nil])
+       (fact "squashes first and second cells when squashable"
              (shift [:a :b :c :d] #(= [:a :b] [%1 %2]) (constantly :ab)) => [:ab :c :d nil]))
